e358b7850116248c1d5fa6f3a07e5dbbeb41b428
凤翼烈风天翔 <zwb843536229@gmail.com>
2022-03-03 16:52:55 +0800

    feat: img onclick

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-let head = document.querySelector('h1')
-head.textContent = 'Hello world!'
+let head = document.querySelector('h1');
+head.textContent = 'Hello world!';
+
+let image = document.querySelector('img');
+
+let next = new Map([['images/firefox-icon.png', 'images/youmu.jpg'], ['images/youmu.jpg', 'images/firefox-icon.png']]);
+image.onclick = function () {
+    let src = image.getAttribute('src');
+    image.setAttribute(next[src]);
+}
