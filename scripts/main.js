6ab5255a084db688029ee42696375bc797b73c7b
凤翼烈风天翔 <zwb843536229@gmail.com>
2022-03-03 17:14:17 +0800

    bugfix:

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,5 @@ let image = document.querySelector('img');
 let next = new Map([['images/firefox-icon.png', 'images/youmu.jpg'], ['images/youmu.jpg', 'images/firefox-icon.png']]);
 image.onclick = function () {
     let src = image.getAttribute('src');
-    image.setAttribute(next[src]);
+    image.setAttribute('src', next[src]);
 }
